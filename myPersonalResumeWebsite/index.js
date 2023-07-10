413220ddf558670c5585c3d7bb3e5007feaacc55
ItsmeZeir <139132577+ItsmeZeir@users.noreply.github.com>
2023-07-11 02:01:01 +0800

    Add files via upload

--- myPersonalResumeWebsite/index.js
+++ myPersonalResumeWebsite/index.js
@@ -1,26 +1,26 @@
-function openTab(tabname){
-    let tablinks = document.getElementsByClassName("tab-links");
-    let tabcontents = document.getElementsByClassName("tab-contents")
+function openTab(tabname) {
+  let tablinks = document.getElementsByClassName("tab-links");
+  let tabcontents = document.getElementsByClassName("tab-contents");
 
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
 
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
 }
 
-function openMenu(){
-    let sideMenu = document.getElementById("sideMenu");
+function openMenu() {
+  let sideMenu = document.getElementById("sideMenu");
 
-    sideMenu.style.right = "0"
+  sideMenu.style.right = "0";
 }
 
-function closeMenu(){
-    let sideMenu = document.getElementById("sideMenu");
+function closeMenu() {
+  let sideMenu = document.getElementById("sideMenu");
 
-    sideMenu.style.right = "-200px"
+  sideMenu.style.right = "-200px";
 }
