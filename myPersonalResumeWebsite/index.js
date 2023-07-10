9d0c1a4b5c2e7d851593e0199c62f7cd16e2f761
ItsmeZeir <139132577+ItsmeZeir@users.noreply.github.com>
2023-07-11 00:02:30 +0800

    Add files via upload

---
+++ myPersonalResumeWebsite/index.js
@@ -0,0 +1,26 @@
+function openTab(tabname){
+    let tablinks = document.getElementsByClassName("tab-links");
+    let tabcontents = document.getElementsByClassName("tab-contents")
+
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+function openMenu(){
+    let sideMenu = document.getElementById("sideMenu");
+
+    sideMenu.style.right = "0"
+}
+
+function closeMenu(){
+    let sideMenu = document.getElementById("sideMenu");
+
+    sideMenu.style.right = "-200px"
+}
